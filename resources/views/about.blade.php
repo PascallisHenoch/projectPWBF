a08d548faaecb86a5866b6a53cdb536757747928
PascallisHenoch <pascallishenoch9@gmail.com>
2021-09-28 23:59:58 +0700

    Revisi Tampilan

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,12 +3,14 @@
 @section('container')
     <h1>About</h1>
 
-    <img src="img/{{ $paskal1 }}" alt="{{ $paskal1 }}" width="150" class="img-thumbnail rounded-circle">
-    <h3>{{ $nama1 }}</h3>
-    <p>{{ $nim1 }}</p>
-    <img src="img/{{ $dhiba1 }}" alt="{{ $dhiba1 }}" width="150" class="img-thumbnail rounded-circle">
-    <h3>{{ $nama2 }}</h3>
-    <p>{{ $nim2 }}</p>
+    <article class="my-5">
+        <img src="img/{{ $paskal1 }}" alt="{{ $paskal1 }}" width="150" class="img-thumbnail rounded-circle">
+        <h4>{{ $nama1 }}</h4>
+        <p>{{ $nim1 }}</p>
+        <img src="img/{{ $dhiba1 }}" alt="{{ $dhiba1 }}" width="150" class="img-thumbnail rounded-circle">
+        <h4>{{ $nama2 }}</h4>
+        <p>{{ $nim2 }}</p>
+    </article>
     
 @endsection
     
--- resources/views/informasi.blade.php
+++ resources/views/informasi.blade.php
@@ -2,12 +2,14 @@
 
 @section('container')
     <h1>Informasi</h1>
-    <img src="img/{{ $image1 }}" alt="{{ $image1 }}">
-    @foreach ($posts as $post)
-    <article class="mb-5">
-        <a href="/posts/{{ $post["slug"] }}"><h2>{{ $post["title"] }}</a>
-        <h5>By: {{ $post["author"] }}</h5>
-        <p>{{ $post["body"] }}</p>
+    <article class="my-4">
+        <img src="img/{{ $image1 }}" alt="{{ $image1 }}">
+        @foreach ($posts as $post)
+        <article class="my-3">
+            <a href="/posts/{{ $post["slug"] }}"><h2>{{ $post["title"] }}</a>
+            <h5>By: {{ $post["author"] }}</h5>
+            <p>{{ $post["body"] }}</p>
+        </article>
     </article>
     @endforeach
     
