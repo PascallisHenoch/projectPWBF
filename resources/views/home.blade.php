2201a87117d29a4a4425a61e3440a028ce16892c
adhibaAlya <90745935+adhibaAlya@users.noreply.github.com>
2021-10-05 11:48:54 +0700

    commit lagi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts/main')
 
 @section('container')
-
-    <h1>Aplikasi Surveilans & informasi Pencegahan Stunting</h1>
+    <h1>Aplikasi Surveilans & informasi Pencegahan Stunting1</h1>
+    
     <img src="img/{{ $image2 }}" alt="{{ $image2 }}" width="300">
 
 @endsection
