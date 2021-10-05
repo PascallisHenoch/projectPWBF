641aa70c148f8b8247de05a1ee7cb5411f67c387
PascallisHenoch <pascallishenoch9@gmail.com>
2021-10-05 11:51:13 +0700

    Revisi lagi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,8 @@
 
 @section('container')
 
-    <h1>Aplikasi Surveilans & informasi Pencegahan Stunting</h1>
+    <h1>Aplikasi Surveilans & informasi Pencegahan Stunting1</h1>
+
     <img src="img/{{ $image2 }}" alt="{{ $image2 }}" width="300">
 
 @endsection
