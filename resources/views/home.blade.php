ac6a38c0df46bb3f653d87edba8b5cc6739da049
PascallisHenoch <pascallishenoch9@gmail.com>
2021-10-05 18:17:09 +0700

    Revisi Judul

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,6 @@
 
 @section('container')
     <h1>Aplikasi Surveilans & informasi Pencegahan Stunting</h1>
-<<<<<<< HEAD
-=======
-    <h1>Kelompok 4</h1>
-    <h1>Asik asik bisa github</h1>
-    <h1>Semangat yokk</h1>
-
->>>>>>> b8ec6ef7a1ba86a8a052ec8b31b85b18fb37547d
     <img src="img/{{ $image2 }}" alt="{{ $image2 }}" width="300">
 
 @endsection
