10f0369f2577401f3f572482d5b3e10a0ea85714
adhibaAlya <90745935+adhibaAlya@users.noreply.github.com>
2021-10-05 12:45:04 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts/main')
 
 @section('container')
-    <h1>Aplikasi Surveilans & informasi Pencegahan Stunting1 Kelompok 4</h1>
+    <h1>Aplikasi Surveilans & informasi Pencegahan Stunting</h1>
+    <h1>Kelompok 4</h1>
 
     <img src="img/{{ $image2 }}" alt="{{ $image2 }}" width="300">
 
