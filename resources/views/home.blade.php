93ea102e343f770a0eab92ac1edc2855bd82db27
adhibaAlya <90745935+adhibaAlya@users.noreply.github.com>
2021-10-05 11:15:35 +0700

    commit ubah

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts/main')
 
 @section('container')
-    <h1>Aplikasi Surveilans & informasi Pencegahan Stunting</h1>
+    <h1>Aplikasi Surveilans & informasi Pencegahan Stunting1</h1>
     
     <img src="img/{{ $image2 }}" alt="{{ $image2 }}" width="300">
 
