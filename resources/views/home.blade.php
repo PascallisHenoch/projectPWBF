375eacc6d8cbf2f28c173a9e73867dae1080ba53
PascallisHenoch <pascallishenoch9@gmail.com>
2021-10-05 11:30:51 +0700

    Revisi Lagi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,8 @@
 @extends('layouts/main')
 
 @section('container')
-<<<<<<< HEAD
+
     <h1>Aplikasi Surveilans & informasi Pencegahan Stunting</h1>
-    <h1>Kelompok 4 PWBF Stunting</h1>
-=======
->>>>>>> 93ea102e343f770a0eab92ac1edc2855bd82db27
     <img src="img/{{ $image2 }}" alt="{{ $image2 }}" width="300">
 
 @endsection
