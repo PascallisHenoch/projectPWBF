b8ec6ef7a1ba86a8a052ec8b31b85b18fb37547d
adhibaAlya <90745935+adhibaAlya@users.noreply.github.com>
2021-10-05 16:55:48 +0700

    homecoba

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
     <h1>Aplikasi Surveilans & informasi Pencegahan Stunting</h1>
     <h1>Kelompok 4</h1>
     <h1>Asik asik bisa github</h1>
+    <h1>Semangat yokk</h1>
 
     <img src="img/{{ $image2 }}" alt="{{ $image2 }}" width="300">
 
