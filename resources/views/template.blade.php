d6bb787fded5ca977174686ff93e791fc1ab5b77
adhibaAlya <90745935+adhibaAlya@users.noreply.github.com>
2021-10-23 14:02:56 +0700

    revisi dikit

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>ASIPS | {{ $title }}</title>
+  <title>ASIPS |{{ $title }}</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -52,7 +52,7 @@
           <img src="{{ asset('admin/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Adhiba Alya</a>
+          <a href="#" class="d-block">Kelompok 4</a>
         </div>
       </div>
 
