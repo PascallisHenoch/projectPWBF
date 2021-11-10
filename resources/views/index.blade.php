92b68c281921793baf1a3702ae075bae15698179
PascallisHenoch <pascallishenoch9@gmail.com>
2021-11-10 09:42:17 +0700

    active

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -52,14 +52,14 @@
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo me-auto nav-link scrollto"><a href="#">ASIPS</a></h1>
+      <h1 class="logo me-auto"><a href="#">ASIPS</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index." class="logo me-auto"><img src="img/logo.png" alt="" class="img-fluid"></a>-->
 
       <nav id="navbar" class="navbar order-last order-lg-0">
         <ul>
-          <li><a class="nav-link scrollto active" href="#">Home</a></li>
-          <li><a class="nav-link scrollto active" href="#about">Edukasi</a></li>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">Edukasi</a></li>
           <li><a class="nav-link scrollto" href="#departments">Informasi</a></li>
           <li><a class="nav-link scrollto" href="#services">Pengguna</a></li>
           <li><a class="nav-link scrollto" href="#gallery">Galeri</a></li>
