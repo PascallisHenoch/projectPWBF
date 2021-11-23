6e1b0e146e32d266db0f0605926c11b941a4b323
PascallisHenoch <pascallishenoch9@gmail.com>
2021-11-23 21:47:17 +0700

    isi admin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('container')
 
 <br/>
-<b><h2><strong><p align="center">Aplikasi Surveilans & informasi Pencegahan Stunting</h2></b></strong>
+<b><h4><strong>Proyek Praktikum PBWF Kelompok 4 ASIPS</h4></b></strong>
 
     <h5></h5>
     <script src="js/script.js"></script>
@@ -11,50 +11,39 @@
     
     <html>
       <font face="Sans-serif">
-        <body><p style="text-indent: 40px;">
+        <body>
+          <b>Bidang</b> <br>
+          Kesehatan
+        </body> 
+        <p></p>
+        <body>
+          <b>Gambaran Sistem</b> <br>
           Aplikasi Surveilans & informasi Pencegahan Stunting) adalah aplikasi tepat guna yang 
           dapat menghubungkan antara kader posyandu dengan pihak puskesmas dan orang tua dalam pencatatan 
           status gizi balita sebagai upaya pencegahan stunting. Aplikasi ini bertujuan untuk melacak, 
           mencatat dan merekam status gizi balita secara tepat, cepat dan sesuai dengan pengukuran di 
           masing - masing posyandu. Sehingga jika ada balita yang terindikasi mengalami stunting maka 
           langsung terlihat pada rekap laporan yang ada pada masing – masing posyandu.
-        </p></body>
+        </body> 
+        <p></p>
+        <body>
+          <b>Aktor</b> <br>
+          1. Super admin <br>
+          2. Kader posyandu/admin setiap posyandu <br>
+          3. Orang tua balita
+        </p>
+        </body>
+        <p></p>
+        <body>
+          <b>Proses-proses</b> <br>
+          1. Pengelolaan data master (tabel kecamatan, kelurahan, posyandu, role) <br>
+          2. Proses pendaftaran balita (tabel balita) <br>
+          3. Proses pencatatan saat posyandu (tabel history posyandu)
+        </p>
+        </body>
       </font> 
     </html>
     <br/>
-
-    {{-- <img src="img/{{ $image2 }}" alt="{{ $image2 }}" width="300"> --}}
-
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item text-center active">
-            <img src="img/gallery/11.jpg" width="300px" height="400px" alt="First slide">
-          </div>
-          <div class="carousel-item text-center">
-            <h6></h6>
-            <img src="img/gallery/22.png" width="300px" height="400px" alt="Second slide">
-          </div>
-          <div class="carousel-item text-center">
-            <h6></h6>
-            <img src="img/gallery/77.jpeg" width="300px" height="400px" alt="Third slide">
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" style="background-color: black;" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" style="background-color: black;" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-
-      
       
 @endsection
         
